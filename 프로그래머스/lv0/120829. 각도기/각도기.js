43e75128f2f7a9bd8a4f5d37a438d1cc13308fbf
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2023-01-15 21:42:11 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -0,0 +1,15 @@
+function solution(angle){
+    switch(true){
+        case (angle<90 && angle>0):
+            return 1;
+        case (angle===90):
+            return 2;
+        case (angle>90 && angle<180):
+            return 3;
+        case (angle===180):
+            return 4;
+        default:
+            return 0;
+            break;
+    }
+}
