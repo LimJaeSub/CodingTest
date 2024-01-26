4a5020d56377b14170970dde1fadf40491838aed
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2024-01-26 16:54:56 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,18 @@
+function solution(num) {
+    let count = 0;
+    while(num!=1){
+        if(num%2==0){
+            num = num/2
+        }
+        else if(num%2==1){
+            num=(num*3)+1
+        }
+        
+        count++;
+        if(count==500){
+            count=-1
+            break;
+        }
+    }
+    return count;
+}
