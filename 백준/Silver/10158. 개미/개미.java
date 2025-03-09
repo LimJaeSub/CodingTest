e16758bf66d73761c8e642d2bc2f2643db916bda
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2025-03-10 00:37:40 +0900

    [Silver III] Title: 개미, Time: 132 ms, Memory: 16204 KB -BaekjoonHub

---
+++ 백준/Silver/10158. 개미/개미.java
@@ -0,0 +1,70 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String a = bf.readLine();
+        String b = bf.readLine();
+        int time = Integer.parseInt(bf.readLine());
+
+        String[] map = a.split(" ");
+        String[] start = b.split(" ");
+
+        int x_max = Integer.parseInt(map[0]);
+        int y_max = Integer.parseInt(map[1]);
+
+        int x_start = Integer.parseInt(start[0]);
+        int y_start = Integer.parseInt(start[1]);
+
+
+        int result_x =0;
+        int result_y =0;
+
+        int k = 0;
+
+        if(x_start+time>x_max){
+
+                int roadx = time-(x_max-x_start);
+                if((roadx/x_max)%2==0){
+                    // 짝수일경우 음수방향으로 이동
+                    result_x=x_max-(roadx%x_max);
+                }
+                else if((roadx/x_max%2)!=0){
+                    // 홀수일 경우 양수 방향으로 이동
+                    result_x=roadx%x_max;
+                }
+        }
+        else{
+            result_x=x_start+time;
+        }
+
+        int w = 0;
+        if(y_start+time>y_max){
+            int roady = time-(y_max-y_start);
+            if((roady/y_max)%2==0){
+                // 짝수일경우 음수방향으로 이동
+                result_y=y_max-(roady%y_max);
+            }
+            else if((roady/y_max%2)!=0){
+                // 홀수일 경우 양수 방향으로 이동
+                result_y=roady%y_max;
+            }
+        }
+
+        else{
+            result_y = y_start+time;
+        }
+
+
+
+
+
+
+
+        bw.write(result_x+" "+result_y);
+        bw.flush();
+        bw.close();
+    }
+}
