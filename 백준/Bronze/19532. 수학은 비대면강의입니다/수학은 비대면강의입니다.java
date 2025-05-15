285a75f78deaf63aa0247d661842a72789a44bad
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2025-05-15 18:00:10 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 128 ms, Memory: 16272 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int x1 = arr[0]; // x
+        int y1 = arr[1]; // y
+        int func1 = arr[2]; // 상수항
+
+        int x2 = arr[3]; // x
+        int y2 = arr[4]; // y
+        int func2 = arr[5]; // 상수항
+
+        int D = x1 * y2 - y1 * x2;
+
+        int find_x = (func1 * y2 - y1 * func2) / D;
+        int find_y = (x1 * func2 - func1 * x2) / D;
+
+        System.out.println(find_x + " " + find_y);
+    }
+}
