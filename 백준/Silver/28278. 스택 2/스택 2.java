f1243513019752975d9214409c351cf827bda9f4
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2025-05-19 15:35:54 +0900

    [Silver IV] Title: 스택 2, Time: 1088 ms, Memory: 313616 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -1,49 +1,38 @@
 import java.util.*;
 import java.io.*;
 
-
 public class Main {
     static List<Integer> stack = new ArrayList<>();
 
-
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         int TC = Integer.parseInt(bf.readLine());
 
-        for(int i=0;i<TC;i++){
-            int[] input = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            switch(input[0]){
-                case 1 :
-                    stack.add(input[1]);
+        for (int i = 0; i < TC; i++) {
+            String[] parts = bf.readLine().split(" ");
+            int cmd = Integer.parseInt(parts[0]);
+
+            switch (cmd) {
+                case 1:
+                    stack.add(Integer.parseInt(parts[1]));
                     break;
-                case 2 :
-                    if(stack.isEmpty()){
-                        System.out.println(-1);
-                    }
-                    else{
-                        int out = stack.get(stack.size()-1);
-                        stack.remove(stack.size()-1);
-                        System.out.println(out);
-                    }
+                case 2:
+                    sb.append(stack.isEmpty() ? -1 : stack.remove(stack.size() - 1)).append("\n");
                     break;
-                case 3 :
-                    System.out.println(stack.size());
+                case 3:
+                    sb.append(stack.size()).append("\n");
                     break;
-                case 4 :
-                    System.out.println(stack.isEmpty() ? 1 : 0);
+                case 4:
+                    sb.append(stack.isEmpty() ? 1 : 0).append("\n");
                     break;
-                case 5 :
-                    if(stack.isEmpty()){
-                        System.out.println(-1);
-                    }
-                    else{
-                        System.out.println(stack.get(stack.size()-1));
-                    }
+                case 5:
+                    sb.append(stack.isEmpty() ? -1 : stack.get(stack.size() - 1)).append("\n");
                     break;
             }
-
         }
 
-
+        System.out.print(sb);
     }
 }
